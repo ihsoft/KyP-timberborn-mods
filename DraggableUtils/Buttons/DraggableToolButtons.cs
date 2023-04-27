a0482d45767e321c256cf8c4ba848b61b26a4132
Igor Zavoychinskiy <igor.zavoychinskiy@gmail.com>
2023-04-26 23:00:25 -0700

    Add into the stock group if possible

--- DraggableUtils/Buttons/DraggableToolButtons.cs
+++ DraggableUtils/Buttons/DraggableToolButtons.cs
@@ -1,7 +1,10 @@
+using System.Collections.Generic;
 using DraggableUtils.Factorys;
 using DraggableUtils.Tools;
+using KSPDev.ProcessingUtils;
 using TimberApi.AssetSystem;
 using Timberborn.BottomBarSystem;
+using Timberborn.BuilderPrioritySystemUI;
 using Timberborn.ToolSystem;
 using UnityEngine;
 
@@ -21,12 +24,24 @@ namespace DraggableUtils.Buttons
 
         private readonly IAssetLoader _assetLoader;
 
+        readonly ToolButtonService _toolButtonService;
+
+        // This is a bad way of doing things, actually. But if that's what you need, then it's a good way.
+        static readonly ReflectedField<ToolButtonService, List<ToolGroupButton>>
+                // ReSharper disable once InconsistentNaming
+                ToolButtonServiceTool_GroupButtons_Field = new("_toolGroupButtons");
+        static readonly ReflectedField<ToolGroupButton, ToolGroup>
+                // ReSharper disable once InconsistentNaming
+                ToolGroupButton_ToolGroup_Field = new("_toolGroup");
+
         public DraggableToolButtons(
             DraggableUtilsGroup draggableUtilsGroup,
             ToolButtonFactory toolButtonFactory,
             ToolGroupButtonFactory toolGroupButtonFactory,
             DraggableToolFactory draggableToolFactory,
-            IAssetLoader assetLoader, HaulPrioritizeTool haulPrioritizeTool)
+            IAssetLoader assetLoader,
+            HaulPrioritizeTool haulPrioritizeTool,
+            ToolButtonService toolButtonService)
         {
             this._draggableUtilsGroup = draggableUtilsGroup;
             this._toolButtonFactory = toolButtonFactory;
@@ -34,26 +49,44 @@ namespace DraggableUtils.Buttons
             this._draggableToolFactory = draggableToolFactory;
             this._assetLoader = assetLoader;
             this._haulPrioritizeTool = haulPrioritizeTool;
+            _toolButtonService = toolButtonService;
         }
         
         
-        public BottomBarElement GetElement()
-        {
+        public BottomBarElement GetElement() {
+            // Attempt to inject the new controls into the stock group first. 
+            if (ToolButtonServiceTool_GroupButtons_Field.IsValid() && ToolGroupButton_ToolGroup_Field.IsValid()) {
+                var stockGroups = ToolButtonServiceTool_GroupButtons_Field.Get(_toolButtonService);
+                foreach (var stockGroup in stockGroups) {
+                    var toolGroup = ToolGroupButton_ToolGroup_Field.Get(stockGroup);
+                    if (toolGroup is BuilderPriorityToolGroup) {
+                        Debug.Log("Attaching DraggableUtils buttons to the stock priority tool group");
+                        AddButtonsToGroup(stockGroup);
+                        return BottomBarElement.CreateMultiLevel(stockGroup.Root, stockGroup.ToolButtonsElement);
+                    }
+                }
+            }
+
+            // No luck, but we still can create our own tool group button.
+            Debug.LogWarningFormat(
+                    "Cannot add draggable tool buttons into the stock priority group. Using own button.");
             _draggableUtilsGroup.LoadIcon(_assetLoader);
-            ToolGroupButton blue = this._toolGroupButtonFactory.CreateBlue((ToolGroup) this._draggableUtilsGroup);
-
-            //Pause buildings
-            this.AddTool((Tool) this._draggableToolFactory.CreatePauseTool(), _assetLoader.Load<Sprite>("DraggableUtils/UI_Buttons/pause_button"), blue);
-
-            //Prioritize haulers
-            this.AddTool(this._draggableToolFactory.CreateHaulPrioritizeTool(), _assetLoader.Load<Sprite>("DraggableUtils/UI_Buttons/hauler_button"), blue);
-
-            //Empty storage
-            this.AddTool(this._draggableToolFactory.CreateEmptyStorageTool(), _assetLoader.Load<Sprite>("DraggableUtils/UI_Buttons/storage_empty_button"), blue);
-
+            var blue = _toolGroupButtonFactory.CreateBlue(_draggableUtilsGroup);
+            AddButtonsToGroup(blue);
             return BottomBarElement.CreateMultiLevel(blue.Root, blue.ToolButtonsElement);
         }
-        
+
+        void AddButtonsToGroup(ToolGroupButton group) {
+            //Pause buildings
+            AddTool((Tool) this._draggableToolFactory.CreatePauseTool(), _assetLoader.Load<Sprite>("DraggableUtils/UI_Buttons/pause_button"), group);
+
+            //Prioritize haulers
+            AddTool(this._draggableToolFactory.CreateHaulPrioritizeTool(), _assetLoader.Load<Sprite>("DraggableUtils/UI_Buttons/hauler_button"), group);
+
+            //Empty storage
+            AddTool(this._draggableToolFactory.CreateEmptyStorageTool(), _assetLoader.Load<Sprite>("DraggableUtils/UI_Buttons/storage_empty_button"), group);
+        }
+
         private void AddTool(Tool tool, Sprite sprite, ToolGroupButton toolGroupButton)
         {
             ToolButton button = this._toolButtonFactory.Create(tool, sprite, toolGroupButton.ToolButtonsElement);
---
+++ DraggableUtils/Utils/ReflectedField.cs
@@ -0,0 +1,55 @@
+﻿// Kerbal Development tools.
+// Author: igor.zavoychinskiy@gmail.com
+// This software is distributed under Public domain license.
+
+using System.Reflection;
+using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+namespace KSPDev.ProcessingUtils {
+
+/// <summary>Wrapper to implement efficient access to the class fields via reflection.</summary>
+/// <remarks>It ignores the access scope. Use <see cref="IsValid"/> to determine if the reflection was successful.
+/// </remarks>
+/// <example>
+/// Below is an accessor to deal with a boolean field, named "isTrueField", from type <c>MyType</c>:
+/// <code><![CDATA[
+/// static readonly ReflectedField<MyType, bool> check = new("isTrueField");
+/// ]]></code>
+/// </example>
+/// <typeparam name="T">type of the class. It's inferred at the instantiation.</typeparam>
+/// <typeparam name="TV">type of the field value.</typeparam>
+public class ReflectedField<T, TV> {
+  readonly FieldInfo _fieldInfo;
+
+  public ReflectedField(string fieldName) {
+    _fieldInfo = typeof(T).GetField(fieldName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+    if (_fieldInfo == null) {
+      Debug.LogErrorFormat("Cannot obtain field {0} from {1}", fieldName, typeof(T));
+    }
+  }
+
+  /// <summary>Indicates if the target field was found and ready to use.</summary>
+  public bool IsValid() {
+    return _fieldInfo != null;
+  }
+
+  /// <summary>Gets the field value or returns a default value if the field is not found.</summary>
+  public TV Get(T instance) {
+    return _fieldInfo != null ? (TV)_fieldInfo.GetValue(instance) : default(TV);
+  }
+
+  /// <summary>Gets the field value or returns the provided default value if the field is not found.</summary>
+  public TV Get(T instance, TV defaultValue) {
+    return _fieldInfo != null ? (TV)_fieldInfo.GetValue(instance) : defaultValue;
+  }
+
+  /// <summary>Sets the field value or does nothing if the field is not found.</summary>
+  public void Set(T instance, TV value) {
+    if (_fieldInfo != null) {
+      _fieldInfo.SetValue(instance, value);
+    }
+  }
+}
+
+}
